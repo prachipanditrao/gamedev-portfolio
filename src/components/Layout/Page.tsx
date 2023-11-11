ddf3b41aa968ae90a500b4a82b43a60e1f309d1a
Ketan Shukla <ketan.shukla87@gmail.com>
2023-11-11 19:00:03 +0530

    lint fixes

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,9 +1,9 @@
-import {PropsWithChildren, memo} from 'react';
-
-import Head from 'next/head';
-import {HomepageMeta} from '../../data/dataDef';
 import {NextPage} from 'next';
+import Head from 'next/head';
 import {useRouter} from 'next/router';
+import {memo,PropsWithChildren} from 'react';
+
+import {HomepageMeta} from '../../data/dataDef';
 
 const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, description}) => {
   const {asPath: pathname} = useRouter();

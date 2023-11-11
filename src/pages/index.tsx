1ab3051a5d167d2eeb9c6200e3d61f4f4b4e691f
Ketan Shukla <ketan.shukla87@gmail.com>
2023-11-11 12:09:44 +0530

    added vercel analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import {Analytics} from '@vercel/analytics/react';
 import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';
 
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
-import Footer from '../components/Sections/Footer';
+// import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
 import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
@@ -25,7 +26,8 @@ const Home: FC = memo(() => {
       <Resume />
       <Testimonials />
       <Contact />
-      <Footer />
+      {/* <Footer /> */}
+      <Analytics />
     </Page>
   );
 });

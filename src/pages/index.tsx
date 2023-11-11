dee0cedb6a056788a8a0f02e4f5c0980db7afbaf
Ketan Shukla <ketan.shukla87@gmail.com>
2023-11-11 16:01:17 +0530

    yarn add @vercel/analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import {Analytics} from '@vercel/analytics/react';
 import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';
 
 import Page from '../components/Layout/Page';
 import About from '../components/Sections/About';
 import Contact from '../components/Sections/Contact';
-// import Footer from '../components/Sections/Footer';
+import Footer from '../components/Sections/Footer';
 import Hero from '../components/Sections/Hero';
 import Portfolio from '../components/Sections/Portfolio';
 import Resume from '../components/Sections/Resume';
@@ -26,8 +25,7 @@ const Home: FC = memo(() => {
       <Resume />
       <Testimonials />
       <Contact />
-      {/* <Footer /> */}
-      <Analytics />
+      <Footer />
     </Page>
   );
 });
